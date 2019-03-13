710e210ee37277f8eff214a382c93dafc5e559e6
jhuizy <jhuizenga99@gmail.com>
2019-03-13 23:32:40 +0800

    removed more junk

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,10 @@
 module.exports = function(eleventyConfig) {
   // Copy the `img/` directory 
   eleventyConfig.addPassthroughCopy("assets");
-  eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("less");
-  eleventyConfig.addPassthroughCopy("node_modules");
   return {
     passthroughFileCopy: true
   };
