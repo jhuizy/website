ef53d18886472b4ceb4542cb542b300b2f202736
jhuizy <jhuizenga99@gmail.com>
2019-03-14 08:51:36 +0800

    attmepts to get things to play together nicely

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,10 @@
 module.exports = function(eleventyConfig) {
+  // fix urls for parcel
+  eleventyConfig.addFilter("urlFix", function(value) {
+    value = !value || value === "" || value === "/" ? "index" : value;
+    return value.indexOf(".html") < 0 ? value + ".html" : value;
+  });
+
   // Copy the `img/` directory 
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("images");
